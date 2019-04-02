e7360328890e2a22944cb0884f815403fee08100
Zalkifl <zcksyed3@gmail.com>
2019-04-02 18:20:45 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'; 
+import FileUploader from 'react-firebase-file-uploader';
+import firebase from 'firebase';
+import config from './firebase_config';
 import './App.css';
 
+firebase.initializeApp(config)
+
 class App extends Component {
+
+  state = {
+    image: '',
+    imageURL: '',
+    progress: 0
+  }
+
+  handleUploadStart = () => {
+
+    this.setState({
+
+      progress: 0
+    })
+  }
+
+  handleUploadSuccess = filename => {
+
+    this.setState({
+
+      image: filename,
+      progress: 100
+    })
+
+    firebase.storage().ref('fire').child(filename).getDownloadURL()
+      .then(url => this.setState({
+          imageURL: url
+      }))
+  }
+
+  handleProgress = progress => {
+    this.setState({
+
+      progress: progress
+    })
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+
+        <label> Progress:</label>
+          <p>{this.state.progress}</p>
+
+          <br/>
+          <br/>
+          <br/>
+          <label> Image:</label>
+            {this.state.image && <img src={this.state.imageURL} />}
+
+            {this.state.imageURL && <a href={this.state.imageURL}> Download me</a>}
+            <br/>
+            <br/>
+            <br/>
+        <FileUploader 
+          accept="image/*"
+          name='image'
+          storageRef={firebase.storage().ref('fire')}
+          onUploadStart={this.handleUploadStart}
+          onUploadSuccess={this.handleUploadSuccess}
+          onProgress={this.handleProgress}
+
+        
+        />
       </div>
     );
   }
